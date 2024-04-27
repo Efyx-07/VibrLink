f4072beafd412c7627e46311f8e393b6132e7f02
Efyx-07 <fx.savary@orange.fr>
2024-04-27 03:04:02 +0200

    settings

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,18 @@
+import { useUserStore } from '../stores';
 
 const HomePage = () => {
+    const userStore = useUserStore();
 
     return (
         <div>
+            {userStore.isLoggedIn ? (
+                <p>Logged in</p>
+            ) : (
+                <p>Not logged in</p>
+            )}
             <h1>I am HomePage</h1>
         </div>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
