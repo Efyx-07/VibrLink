b30c73638c7f031cbaa34e5f670add95453cca3e
François-Xavier Savary <fx.savary@orange.fr>
2024-04-27 18:20:25 +0200

    components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
-import HomePage from "./pages/HomePage";
+import HomePage from './pages/HomePage';
+import Header from './components/Header/Header';
 
 const App = () => {
 
   return (
     <div>
+      <Header />
       <Routes>
         <Route index path="/" element={<HomePage />} />
       </Routes>
