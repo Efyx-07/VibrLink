1ad19410c645468e545f5ef55076c98506700e58
François-Xavier Savary <fx.savary@orange.fr>
2024-04-28 15:00:30 +0200

    login form 1

--- src/components/user-forms/LoginForm.tsx
+++ src/components/user-forms/LoginForm.tsx
@@ -1,39 +1,61 @@
-interface Field {
-    label: Label;
-    input: Input;
-};
-
-interface Label {
-    for: string;
-    name: string;
-};
-
-interface Input {
-    type: string;
-    name: string;
-    value: string;
-    onChange: React.ChangeEventHandler<HTMLInputElement>;
-    class: string | null;
-};
-
-function Field({label, input}: {label: Label, input: Input}) {
-    return (
-        <div className="field">
-            <label htmlFor={label.for}>{label.name}</label>
-            <input 
-                type={input.type}
-                name={input.name}
-                value={input.value}
-                onChange={input.onChange}
-                className={input.class || ''}
-            />
-        </div>
-    )
-};
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useUserStore, useGlobalDataStore } from "../../stores";
 
 export default function LoginForm() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const userStore = useUserStore();
+    const {hostName} = useGlobalDataStore();
+    const navigate = useNavigate();
+
+    const userLogin = async(e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault();
+
+        try {
+
+            const response: Response = await fetch(`${hostName}/user/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                }) 
+            });
+            
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data.message);
+                userStore.setUserData(data.user);
+
+                const token = data.token;
+                localStorage.setItem('token', token);
+                userStore.setToken(token);
+                navigate('/')
+            } else {
+                console.error('Error while connecting: ', response.statusText);
+            }
+
+        } catch (error) {
+            console.error('Error while connecting: ', error);
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={userLogin}>
+            <div className="input-container">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="userlogin_email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div className="input-container">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="userlogin_password" className="password-input" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <button type="submit">Log in</button>
         </form>
     )
 };
