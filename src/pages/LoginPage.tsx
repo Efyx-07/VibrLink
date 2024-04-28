81f23ce8b002c227a89b1995adc4581235ad623d
Efyx-07 <fx.savary@orange.fr>
2024-04-28 23:42:02 +0200

    header ok

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,13 +1,15 @@
 import LoginForm from "../components/user-forms/LoginForm";
 import '../assets/sass/common/pages-common-styles.scss';
-import styles from './LoginPage.module.scss';
+import './LoginPage.scss';
 
 export default function LoginPage() {
     return (
         <div className="page">
-            <div className={`content ${styles.content}`}>
-                <LoginForm />
-                <p>I forgot my password</p>
+            <div className="content">
+                <div className="form-container">
+                    <LoginForm />
+                    <p className="option">I forgot my password</p>
+                </div>
             </div>
         </div>
     )
