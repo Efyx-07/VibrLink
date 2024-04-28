e44e718723b602240f0e1baf9f8235f7c859827d
François-Xavier Savary <fx.savary@orange.fr>
2024-04-28 16:08:13 +0200

    login form 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Header from './components/header/Header';
+import { useUserStore, useReleaseStore } from './stores';
 
 export default function App() {
+  const userStore = useUserStore();
+  const releaseStore = useReleaseStore();
+
+  useEffect(() => {
+    const initApp = async (): Promise<void> => {
+      try {
+        await userStore.loadUserDataFromLocalStorage();
+
+        const token = localStorage.getItem('token');
+
+        if (token) {
+          userStore.setToken(token);
+          const userId = userStore.user?.id;
+
+          if (userId) {
+            await releaseStore.loadReleasesData(userId);
+          }
+        }
+      } catch (error) {
+        console.error('Error while fetching data: ', error);
+      }
+    };
+
+    initApp();
+  }, []);
 
   return (
     <div>
@@ -16,5 +42,5 @@ export default function App() {
         <Route index path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
-  )
-};
+  );
+}
