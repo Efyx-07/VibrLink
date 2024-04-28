cb191348475c9f20dbdf0ced0ffa4f350af1318a
Efyx-07 <fx.savary@orange.fr>
2024-04-28 03:24:18 +0200

    header ok

--- src/components/user-forms/LoginForm.tsx
+++ src/components/user-forms/LoginForm.tsx
@@ -1,6 +1,40 @@
+interface Field {
+    label: Label;
+    input: Input;
+};
+
+interface Label {
+    for: string;
+    name: string;
+};
+
+interface Input {
+    type: string;
+    name: string;
+    value: string;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    class: string | null;
+};
+
+function Field({label, input}: {label: Label, input: Input}) {
+    return (
+        <div className="field">
+            <label htmlFor={label.for}>{label.name}</label>
+            <input 
+                type={input.type}
+                name={input.name}
+                value={input.value}
+                onChange={input.onChange}
+                className={input.class || ''}
+            />
+        </div>
+    )
+};
+
 export default function LoginForm() {
     return (
-        <form></form>
+        <form>
+        </form>
     )
 };
 
