eead25b92bcaa4fe485fcd936de48e437a683e01
Efyx-07 <fx.savary@orange.fr>
2024-04-29 00:09:39 +0200

    login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,15 +1,35 @@
 import LoginForm from "../components/user-forms/LoginForm";
 import '../assets/sass/common/pages-common-styles.scss';
 import './LoginPage.scss';
+import { useState } from "react";
 
 export default function LoginPage() {
+
+    const [isLoginFormVisible, setIsLoginFormVisible] = useState(true);
+
+    const hideLoginForm = () => {
+        setIsLoginFormVisible(false)
+    };
+
+    const showLoginForm = () => {
+        setIsLoginFormVisible(true)
+    };
     return (
         <div className="page">
             <div className="content">
-                <div className="form-container">
-                    <LoginForm />
-                    <p className="option">I forgot my password</p>
-                </div>
+                {
+                    isLoginFormVisible ?
+
+                    <div className="form-container">
+                        <LoginForm />
+                        <p className="option" onClick={hideLoginForm}>I forgot my password</p>
+                    </div>
+                :
+                    <div className="form-container">
+                        Je suis Form 2
+                        <p className="option" onClick={showLoginForm}>Back to sign in</p>
+                    </div>
+                }    
             </div>
         </div>
     )
