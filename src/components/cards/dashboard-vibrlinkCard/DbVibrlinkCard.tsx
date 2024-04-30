1ff1f19780249526d9ace10f3adae384fa6a791f
François-Xavier Savary <fx.savary@orange.fr>
2024-04-30 16:45:37 +0200

    vibrlinksPage

---
+++ src/components/cards/dashboard-vibrlinkCard/DbVibrlinkCard.tsx
@@ -0,0 +1,6 @@
+export default function DbVibrlinkCard() {
+
+    return (
+        <div className="card"></div>
+    )
+}
--- src/pages/VibrlinksPage.tsx
+++ src/pages/VibrlinksPage.tsx
@@ -1,9 +1,15 @@
+import DbVibrlinkCard from "../components/cards/dashboard-vibrlinkCard/dbVibrlinkCard";
+import '../assets/sass/common/pages-common-styles.scss';
+
 export default function VibrlinksPage() {
 
     return (
         <div className="page">
             <div className="content">
                 <h1>VibrlinksPage</h1>
+                <div className="dbVibrlinkCards-container">
+                    <DbVibrlinkCard />
+                </div>
             </div>
         </div>
     )
