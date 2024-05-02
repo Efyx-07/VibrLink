1978e902d0b7577f7b49600b20b014232b2cf63e
François-Xavier Savary <fx.savary@orange.fr>
2024-05-02 15:17:14 +0200

    lpVibrlinkCard

--- src/components/cards/LpVibrlinkCard.tsx
+++ src/components/cards/LpVibrlinkCard.tsx
@@ -1,4 +1,5 @@
 import { Release } from "../../types/releaseTypes";
+import './LpVibrlinkCard.scss';
 
 interface SelectedReleaseProps {
     selectedRelease: Release;
@@ -6,7 +7,7 @@ interface SelectedReleaseProps {
 
 export default function LpVibrlinkCard({selectedRelease}: SelectedReleaseProps) {
     return (
-        <div className="release-card">
+        <div className="vibrlink-card">
             <ReleaseCoverAndPlayer selectedRelease={selectedRelease}/>
             <ReleaseInfos selectedRelease={selectedRelease}/>
             <ReleaseLinks selectedRelease={selectedRelease}/>
@@ -25,18 +26,24 @@ function ReleaseCoverAndPlayer({selectedRelease}: SelectedReleaseProps) {
 function ReleaseInfos({selectedRelease}: SelectedReleaseProps) {
     return (
         <div className="infos-container">
-            <p className="release-title">{selectedRelease.artist} - {selectedRelease.title}</p>
+            <p className="title">{selectedRelease.artist} - {selectedRelease.title}</p>
             <p className="mention">Choose your platform</p>
+            <div className="triangle"></div>
         </div>
     )
 };
 
 function ReleaseLinks({selectedRelease}: SelectedReleaseProps) {
+
+    const openInANewTab = (url: string): void => {
+        window.open(url, '_blank');
+    }
+
     return (
         <div className="links-container">
             {selectedRelease.platforms.map(platform => (
                 platform.url && platform.visibility ? (
-                    <div className="link-container" key={platform.name}>
+                    <div className="link-container" key={platform.id} onClick={() => platform.url && openInANewTab(platform.url)}>
                         <div className="logo-container">
                             <img src={platform.logoUrl} />
                         </div>
