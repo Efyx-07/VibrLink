5035ea3292a0b9f8f697f5a134272984932b1d5f
François-Xavier Savary <fx.savary@orange.fr>
2024-05-02 13:32:44 +0200

    api getReleaseBySlug

--- src/services/releasesApi.ts
+++ src/services/releasesApi.ts
@@ -3,6 +3,7 @@ import type { ImportMetaEnv } from '../vite-env';
 
 const hostName: ImportMetaEnv = import.meta.env.VITE_BACKEND_URL;
 
+// fetch the releases data by userId 
 export async function fetchReleasesData(userId: number): Promise<Release[]> {
     try {
         
@@ -33,6 +34,39 @@ export async function fetchReleasesData(userId: number): Promise<Release[]> {
         console.log('datas recupérées: ', data.formattedReleases)
         
         return data.formattedReleases;
+
+    } catch (error) {
+        console.error('Error while fetching datas', error);
+        throw error;
+    }
+};
+
+// fetch the release of one data by its slug
+export async function fetchReleaseDataBySlug(releaseSlug: string): Promise<Release> {
+    try {
+
+        const response = await fetch(`${hostName}/releasesRoute/release/${releaseSlug}`);
+
+        if (!response.ok) {
+            throw new Error ('Error while fetching datas of the release');
+        }
+
+        const data = await response.json();
+
+        if (!data.releaseData || data.releaseData.length === 0) {
+            console.error('Invalid response format:', data);
+            throw new Error('Invalid response format');
+        };
+
+         // convert the visibility values from number to boolean (1 = true, 0 = false)
+        const releaseData = data.releaseData[0];
+        releaseData.platforms.forEach((platform: Platform) => {
+            platform.visibility = !!platform.visibility;
+        });
+
+        console.log('datas recupérées: ', releaseData)
+        
+        return releaseData;
 
     } catch (error) {
         console.error('Error while fetching datas', error);
