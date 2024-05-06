f931e1703e97531f670b2979f9af96cce630872b
François-Xavier Savary <fx.savary@orange.fr>
2024-05-06 12:46:56 +0200

    forms field error

--- src/components/user-forms/LoginForm.tsx
+++ src/components/user-forms/LoginForm.tsx
@@ -5,6 +5,7 @@ import { validateData } from "../../utils/validateData";
 import { login } from "../../services/authService";
 import UserFormField from "./UserFormField";
 import FormButton from "../common/FormButton";
+import LoadingSpinner from "../common/LoadingSpinner";
 import '../../assets/sass/common/forms-style.scss';
 
 export default function LoginForm() {
@@ -12,14 +13,17 @@ export default function LoginForm() {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [errorMessage, setErrorMessage] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState(false);
     const userStore = useUserStore();
     const navigate = useNavigate();
 
     const userLogin = async(e: React.FormEvent<HTMLFormElement>): Promise <void> => {
         e.preventDefault();
+        setIsLoading(true);
 
         if (!validateData(email, password)) {
             setErrorMessage(true);
+            setIsLoading(false);
             // if error reset the form after 3s
             setTimeout(() => {
                 setErrorMessage(false);
@@ -40,6 +44,7 @@ export default function LoginForm() {
 
         } catch (error) {
             setErrorMessage(true);
+            setIsLoading(false);
             console.error('Error while connecting: ', error);
         }
     };
@@ -51,25 +56,35 @@ export default function LoginForm() {
     };
 
     return (
-        <form onSubmit={userLogin}>
-            <UserFormField 
-                label="Email address" 
-                type="email" 
-                name="email" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-            />
-            <UserFormField 
-                label="Enter your password" 
-                type="password" 
-                name="password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)} 
-                className="password-input" 
-            />
-            {errorMessage && <p className="error-message">Wrong email or password</p>}
-            <FormButton type="submit" name="Log in" />
-        </form>
+        <>
+            {isLoading ? (
+                <>
+                    <LoadingSpinner />
+                </>
+                ) 
+                : 
+                (
+                    <form onSubmit={userLogin}>
+                        <UserFormField 
+                            label="Email address" 
+                            type="email" 
+                            name="email" 
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)} 
+                        />
+                        <UserFormField 
+                            label="Enter your password" 
+                            type="password" 
+                            name="password" 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            className="password-input" 
+                        />
+                        {errorMessage && <p className="error-message">Wrong email or password</p>}
+                        <FormButton type="submit" name="Log in" />
+                    </form>
+                )}
+        </>    
     )
 };
 
