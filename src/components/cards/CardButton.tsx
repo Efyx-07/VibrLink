e7b6c94eb2851ba3db145a7d09d75323492e61bd
François-Xavier Savary <fx.savary@orange.fr>
2024-05-07 14:21:26 +0200

    DBLinkCard style

--- src/components/cards/CardButton.tsx
+++ src/components/cards/CardButton.tsx
@@ -2,13 +2,13 @@ import { MouseEventHandler } from "react";
 import { Icon } from '@iconify-icon/react';
 import './CardButton.scss';
 
-interface CardButton {
+interface CardButtonProps {
     name: string;
     icon: string;
     onClick: MouseEventHandler<HTMLDivElement>;
 };
 
-export default function CardButton({name, icon, onClick}: CardButton) {
+export default function CardButton({name, icon, onClick}: CardButtonProps) {
 
     return (
         <div className="card-button" onClick={onClick}>
