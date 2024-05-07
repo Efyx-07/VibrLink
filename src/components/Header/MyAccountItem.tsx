15b87aa4ede78d655248f5d4cf77a6b1cf29e06e
François-Xavier Savary <fx.savary@orange.fr>
2024-05-07 12:53:25 +0200

    Header style

--- src/components/Header/MyAccountItem.tsx
+++ src/components/Header/MyAccountItem.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import { useUserStore } from "../../stores";
 import HoverUserMenu from './HoverUserMenu';
 import './Navigation.scss';
 
 export default function MyAccountItem() {
+
+    const isLogged: boolean = useUserStore(state => state.isLoggedIn);
+
     const [isHoverUserMenuVisible, setIsHoverUserMenuVisible] = useState<boolean>(false);
     const handleEmailMouseEnter = (): void => {
         setIsHoverUserMenuVisible(true);
@@ -14,17 +18,19 @@ export default function MyAccountItem() {
 
     return (
         <div className="navItems-container">
-            <div
-                className="navItem"
-                onMouseEnter={handleEmailMouseEnter}
-                onMouseLeave={handleEmailMouseLeave}
-            >
-                <p>My Account</p>
-                {isHoverUserMenuVisible && 
-                    (
-                        <HoverUserMenu />
-                    )}
-            </div>
+            {isLogged && (
+                <div
+                    className="navItem"
+                    onMouseEnter={handleEmailMouseEnter}
+                    onMouseLeave={handleEmailMouseLeave}
+                >
+                    <p>My Account</p>
+                    {isHoverUserMenuVisible && 
+                        (
+                            <HoverUserMenu />
+                        )}
+                </div>
+            )}  
         </div>
     )
 }
