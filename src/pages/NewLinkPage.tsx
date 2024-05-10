895bdeb08a5270a465959b9759ec93726f8dea0c
François-Xavier Savary <fx.savary@orange.fr>
2024-05-10 16:54:13 +0200

    FormPage

--- src/pages/NewLinkPage.tsx
+++ src/pages/NewLinkPage.tsx
@@ -1,17 +1,19 @@
 import NewLinkForm from "../components/release-forms/NewLinkForm";
+import FormPageMessage from "../components/common/FormPageMessage";
 import DirectionSeparator from "../components/common/DirectionSeparator";
 import '../assets/sass/common/messageAndFormContainer.scss';
 
 export default function NewLinkPage() {
 
+    const mainTextPrimary: string = 'Start';
+    const mainTextSecondary: string = ' by entering your release Spotify Id';
+    const subText: string = 'It will automatically generate a new vibrlink with the Spotify, Deezer and YouTube links. Then, you\'ll be able to add other platforms links and manage them as you want. Let\'s go !'
+
     return(
         <div className="page">
             <div className="content">
                 <div className="message-and-form-container">
-                    <div className="message-container">
-                        <h1><span>Start</span> by entering your release Spotify Id</h1>
-                        <p>It will automatically generate a new vibrlink with the Spotify, Deezer and YouTube links. Then, you'll be able to add other platforms links and manage them as you want. Let's go !</p>
-                    </div>
+                    <FormPageMessage mainTextPrimary={mainTextPrimary} mainTextSecondary={mainTextSecondary} subText={subText} />
                     <DirectionSeparator />
                     <div className="form-container">
                         <NewLinkForm />
