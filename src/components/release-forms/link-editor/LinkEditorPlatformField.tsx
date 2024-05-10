c4ab6a7b627fe3f4c2abdc9ec0bbc2ebcd6e965e
François-Xavier Savary <fx.savary@orange.fr>
2024-05-10 10:57:41 +0200

    style LinkEditor

--- src/components/release-forms/link-editor/LinkEditorPlatformField.tsx
+++ src/components/release-forms/link-editor/LinkEditorPlatformField.tsx
@@ -3,6 +3,7 @@ import { openInANewTab } from "../../../utils/openInANewTab";
 import CustomSwitch from "../../common/CustomSwitch";
 import { Icon } from '@iconify-icon/react';
 import { MouseEventHandler } from "react";
+import './LinkEditorPlatformField.scss';
 import './ActionButton.scss';
 
 interface PlatformFieldProps {
@@ -18,45 +19,90 @@ interface PlatformFieldProps {
 
 export default function LinkEditorPlatformField({platformsWithUrl, platform, newUrls, onChange, platformsVisibility, onVisibilityChange, onAddButtonClick}: PlatformFieldProps) {
     return (
-        <div className="field-wrapper" key={platform.id}>
-            <div className="logo-container">
-                <img src={platform.logoUrl} />
-            </div>
-            <input 
-                type="url" 
-                name="url"
-                id="url"
-                value={newUrls[platform.id] || ""}
-                onChange={(e) => onChange(platform.id, e.target.value)}
-            />
-            <div className="buttons-container">
-                {platformsWithUrl ?
-                    (
-                        <>
-                            <ActionButton 
-                                name="Test link" 
-                                icon="majesticons:open" 
-                                onClick={() => platform.url && openInANewTab(platform.url)} 
+        <>
+            {/* only visible on devices with a width > 830px */}
+            <div className="field-wrapper" key={platform.id}>
+                <div className="logo-container">
+                    <img src={platform.logoUrl} />
+                </div>
+                <input 
+                    type="url" 
+                    name="url"
+                    id="url"
+                    value={newUrls[platform.id] || ""}
+                    onChange={(e) => onChange(platform.id, e.target.value)}
+                />
+                <div className="buttons-container">
+                    {platformsWithUrl ?
+                        (
+                            <>
+                                <ActionButton 
+                                    name="Test link" 
+                                    icon="majesticons:open" 
+                                    onClick={() => platform.url && openInANewTab(platform.url)} 
+                                />
+                                
+                                <CustomSwitch 
+                                    platformVisibility={platformsVisibility[platform.id]} 
+                                    platformId={platform.id} 
+                                    onVisibilityChange={onVisibilityChange}
+                                />
+                            </>                       
+                        )
+                        :
+                        (
+                            <ActionButton
+                                name="Add" 
+                                icon="dashicons:plus" 
+                                onClick={onAddButtonClick} 
                             />
-                            
-                            <CustomSwitch 
-                                platformVisibility={platformsVisibility[platform.id]} 
-                                platformId={platform.id} 
-                                onVisibilityChange={onVisibilityChange}
-                            />
-                        </>                       
-                    )
-                    :
-                    (
-                        <ActionButton
-                            name="Add" 
-                            icon="dashicons:plus" 
-                            onClick={onAddButtonClick} 
-                        />
-                    )
-                }
+                        )
+                    }
+                </div>
             </div>
-        </div>
+            {/* only visible on devices with a width < 830px */}
+            <div className="mobile-field-wrapper" key={platform.id}>
+                <div className="logo-and-buttons-container">
+                    <div className="logo-container">
+                        <img src={platform.logoUrl} />
+                    </div>
+                    <div className="buttons-container">
+                        {platformsWithUrl ?
+                            (
+                                <>
+                                    <ActionButton 
+                                        name="Test link" 
+                                        icon="majesticons:open" 
+                                        onClick={() => platform.url && openInANewTab(platform.url)} 
+                                    />
+                                    
+                                    <CustomSwitch 
+                                        platformVisibility={platformsVisibility[platform.id]} 
+                                        platformId={platform.id} 
+                                        onVisibilityChange={onVisibilityChange}
+                                    />
+                                </>                       
+                            )
+                            :
+                            (
+                                <ActionButton
+                                    name="Add" 
+                                    icon="dashicons:plus" 
+                                    onClick={onAddButtonClick} 
+                                />
+                            )
+                        }
+                    </div>
+                </div>
+                <input 
+                    type="url" 
+                    name="url"
+                    id="url"
+                    value={newUrls[platform.id] || ""}
+                    onChange={(e) => onChange(platform.id, e.target.value)}
+                />
+            </div>
+        </>
     )
 };
 
