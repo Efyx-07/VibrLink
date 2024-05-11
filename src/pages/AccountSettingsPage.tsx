d4c3dc36fbf82a482e7b99fb1c46f3e52828a2ee
François-Xavier Savary <fx.savary@orange.fr>
2024-05-11 10:49:47 +0200

    FormPage

--- src/pages/AccountSettingsPage.tsx
+++ src/pages/AccountSettingsPage.tsx
@@ -1,10 +1,23 @@
-import UpdatePasswordForm from "../components/user-forms/UpdatePasswordForm"
+import FormPageMessage from "../components/common/FormPageMessage";
+import StyledSeparator from "../components/common/StyledSeparator";
+import UpdatePasswordForm from "../components/user-forms/UpdatePasswordForm";
 
 export default function AccountSettingsPage() {
+
+    const mainTextPrimary: string = 'Security';
+    const mainTextSecondary: string = ' first';
+    const subText: string = 'It seems you need to update your password, choose a unique one to improve your security !';
+
     return (
         <div className="page">
             <div className="content">
-                <UpdatePasswordForm />
+                <div className="message-and-form-container">
+                    <FormPageMessage mainTextPrimary={mainTextPrimary} mainTextSecondary={mainTextSecondary} subText={subText} />
+                    <StyledSeparator icon="game-icons:police-officer-head" />
+                    <div className="form-container">
+                        <UpdatePasswordForm />
+                    </div>
+                </div>
             </div>
         </div>
     )
