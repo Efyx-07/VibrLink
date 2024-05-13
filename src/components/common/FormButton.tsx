24f9e0e735493d96f75f28d57c30d6f957f8245b
François-Xavier Savary <fx.savary@orange.fr>
2024-05-13 13:48:00 +0200

    hero component

--- src/components/common/FormButton.tsx
+++ src/components/common/FormButton.tsx
@@ -5,12 +5,13 @@ interface ButtonProps {
     type: "submit" | "reset" | "button" | undefined;
     name: string;
     onClick?: MouseEventHandler<HTMLButtonElement>;
+    id?: string;
 };
 
-export default function FormButton({ type, name, onClick }: ButtonProps) {
+export default function FormButton({ type, name, onClick, id }: ButtonProps) {
 
     return (
-        <button type={type} onClick={onClick} className="form-button">
+        <button type={type} onClick={onClick} className="form-button" id={id}>
             <p>{name}</p>
         </button>
     )
--- src/components/home-page/Hero.tsx
+++ src/components/home-page/Hero.tsx
@@ -1,21 +1,40 @@
+import { useEffect } from 'react';
+import { useUserStore } from '../../stores';
 import { useNavigate } from 'react-router-dom';
 import FormButton from '../common/FormButton';
 import './Hero.scss';
 
 export default function Hero() {
 
+    // get the login status of the user to display the wanted buttons
+    const isLogged: boolean = useUserStore(state => state.isLoggedIn);
+
     const navigate = useNavigate();
+
+    // watch and update the login status of the user
+    useEffect(() => {
+    }, [isLogged]);
 
     return (
         <div className="hero-container">
             <div className="text-container">
-                <h1>Hey it's the <span>big day </span>!<br/>Ready to launch your new music ?</h1>
+                <h1>Hey, it's the <span>big day </span>!<br/>Ready to launch your new music ?</h1>
                 <p>As a musician, we know how special this day is and how exciting it is to share your new release with your fan community. Let's do it!</p>
             </div>
             <div className="buttons-container">
-                <FormButton type="button" name="Create a free account" onClick={() => navigate('/signup')}/>
-                <p>or</p>
-                <FormButton type="button" name="Sign in" onClick={() => navigate('/login')}/>
+                {isLogged ? 
+                    (
+                        <FormButton type="button" name="Create a new vibrLink" onClick={() => navigate('/new-vibrlink')} id="hero-button"/>
+                    )
+                    :
+                    (
+                        <>
+                            <FormButton id="hero-button" type="button" name="Create a free account" onClick={() => navigate('/signup')}/>
+                            <p>or</p>
+                            <FormButton id="hero-button" type="button" name="Sign in" onClick={() => navigate('/login')}/>
+                        </>
+                    )
+                }
             </div>
         </div>
     )
