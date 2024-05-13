f1079e8414da56040993f64bbd786e7971cc55ee
François-Xavier Savary <fx.savary@orange.fr>
2024-05-13 13:24:18 +0200

    hero component

--- src/components/home-page/Hero.tsx
+++ src/components/home-page/Hero.tsx
@@ -1,7 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import FormButton from '../common/FormButton';
 import './Hero.scss';
 
 export default function Hero() {
+
+    const navigate = useNavigate();
+
     return (
         <div className="hero-container">
             <div className="text-container">
@@ -9,9 +13,9 @@ export default function Hero() {
                 <p>As a musician, we know how special this day is and how exciting it is to share your new release with your fan community. Let's do it!</p>
             </div>
             <div className="buttons-container">
-                <FormButton type="button" name="Create a free account"/>
+                <FormButton type="button" name="Create a free account" onClick={() => navigate('/signup')}/>
                 <p>or</p>
-                <FormButton type="button" name="Sign in"/>
+                <FormButton type="button" name="Sign in" onClick={() => navigate('/login')}/>
             </div>
         </div>
     )
