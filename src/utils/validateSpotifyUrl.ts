e574b43a2e34eee23c0c558ec529dd254cacdbd3
François-Xavier Savary <fx.savary@orange.fr>
2024-05-23 14:17:42 +0200

    albumUrl validation method changed

--- src/utils/validateSpotifyUrl.ts
+++ src/utils/validateSpotifyUrl.ts
@@ -1,12 +1,13 @@
 // function to check if the album format is correct
 // a valid spotify URL must have this format: https://open.spotify.com/intl-fr/album/ + spotifyId (22 or 48 chars)
-const spotifyPrefix: string = "https://open.spotify.com/intl-fr/album/";
+const spotifyPcPrefix: string = "https://open.spotify.com/intl-fr/album/";
+const spotifyMobilePrefix: string = "https://open.spotify.com/album/";
 const regexString22: RegExp = /^.{22}$/;
 const regexString48: RegExp = /^.{48}$/;
 
 export function validateSpotifyUrl(albumUrl: string): boolean {
-    if (albumUrl.startsWith(spotifyPrefix)) {
-        const spotifyId: string = albumUrl.slice(spotifyPrefix.length);
+    if (albumUrl.startsWith(spotifyPcPrefix) || albumUrl.startsWith(spotifyMobilePrefix) ) {
+        const spotifyId: string = albumUrl.startsWith(spotifyPcPrefix) ? albumUrl.slice(spotifyPcPrefix.length) : albumUrl.slice(spotifyMobilePrefix.length);
 
         if (regexString22.test(spotifyId) || regexString48.test(spotifyId)) {
             return true;
