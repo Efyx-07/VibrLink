ff83a80f80902e5e2a24f962e69fed73500d0acb
François-Xavier Savary <fx.savary@orange.fr>
2024-05-23 15:42:55 +0200

    validateSpotifyUrl: accept urls with country or international prefixes

--- src/utils/validateSpotifyUrl.ts
+++ src/utils/validateSpotifyUrl.ts
@@ -1,15 +1,15 @@
 // function to check if the album format is correct
-// a valid spotify URL must have this format: spotifyPrefix(FR or International) + spotifyId (22 or 48 chars)
-// !!! spotifyPcPrefix !== spotifyMobilePrefix 
+// a valid spotify URL must have this format: spotifyPrefix(Country or International) + spotifyId (22 or 48 chars)
+// !!! spotifyCountryPrefix !== spotifyInternationalPrefix 
 
-const spotifyPcPrefix: string = "https://open.spotify.com/intl-fr/album/";
-const spotifyMobilePrefix: string = "https://open.spotify.com/album/";
+const spotifyCountryPrefixRegex: RegExp = /^https:\/\/open\.spotify\.com\/intl-[a-z]{2}\/album\//; // example: https://open.spotify.com/intl-fr/album/
+const spotifyInternationalPrefix: string = "https://open.spotify.com/album/";
 const regexString22: RegExp = /^.{22}$/;
 const regexString48: RegExp = /^.{48}$/;
 
 export function validateSpotifyUrl(albumUrl: string): boolean {
-    if (albumUrl.startsWith(spotifyPcPrefix) || albumUrl.startsWith(spotifyMobilePrefix) ) {
-        const spotifyId: string = albumUrl.startsWith(spotifyPcPrefix) ? albumUrl.slice(spotifyPcPrefix.length) : albumUrl.slice(spotifyMobilePrefix.length);
+    if (spotifyCountryPrefixRegex.test(albumUrl) || albumUrl.startsWith(spotifyInternationalPrefix)) {
+        const spotifyId: string = spotifyCountryPrefixRegex.test(albumUrl) ? albumUrl.replace(spotifyCountryPrefixRegex, '') : albumUrl.replace(spotifyInternationalPrefix, '');
 
         if (regexString22.test(spotifyId) || regexString48.test(spotifyId)) {
             return true;
